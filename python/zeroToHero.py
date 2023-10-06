3346fc30d611a583b5981d190fb38d753405114e
iemafzalhassan <iemafzalhassan@gmail.com>
2023-10-06 19:00:01 +0000

    Learned python

---
+++ python/zeroToHero.py
@@ -0,0 +1,121 @@
+                            # variables
+
+#    A entity of a program that holds a value.
+
+x = 90
+
+# here x :- is variable 90 is the content or the value of the variable.
+
+#            ---> Data types <---
+
+# Type of value which we assign to the variable.
+
+
+#            ---> Integer <---
+
+
+x = 95
+print(x)
+type(x)
+
+
+#             ---> Float <---
+
+x = 57.90
+type(x)
+print(x)
+
+#             ---> String    --- collection of character.
+
+x = "you're most welcomed here"
+type(x)
+
+print(x)
+
+        #  or use can use single quotes.
+
+x = 'This repo is especially designed for you'
+type(x)
+print(x)
+
+
+#              ---> lists <---
+
+# lists is there we can assign multiple value or in just simple word collection of value.
+
+
+x = [14, 67, 9]
+type(x)
+print(x)
+
+# there is multiple value stored in a single variable so we can use [ ] to extract the vlaue.
+
+print(x[0])
+print(x[1])
+print(x[2])
+
+#  output will ---> 14, 67,9
+
+
+# To change the value 
+x[0]=91
+
+# output was 14
+# now the output is ---> 91
+print(x)
+
+#   output will be ---> 91, 67, 9
+
+#            --->  Tuple   <---
+
+x = (4, 8, 6)
+print(x)
+#   output -->  4, 8, 6
+
+#       check the type of x
+
+type(x)
+# output -->   tuple
+
+
+# Difference between Lists and tuple are --->
+
+#       ---> Tuples are immutable.
+#       ---> Lists are mutable.
+
+
+# let's understand it with an example.
+
+            # list
+x = [ 16, 21, 23]
+x[0]=95
+print(x)
+
+#   output will be [ 95 21 23]
+
+            # Tuple
+x = (16, 21, 23)
+x[2]=90
+print(x)
+
+# output will be TypeError: 'tuple' object does not support item assignment.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # Boolean
