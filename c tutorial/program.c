c96d7b13063d11ab9503a7dca5d6d1f8a0d6e10e
iemafzalhassan <iemafzalhassan@gmail.com>
2023-10-09 22:53:46 +0530

    corrected main.c

---
+++ c tutorial/program.c
@@ -0,0 +1,119 @@
+#include<stdio.h>
+int main() {
+
+	// This is a single line comment in C
+	//you must use comments to explain your code
+	// it will help to you back and the other people to understand your code.
+	// and we can also understand our code later
+
+
+	/*
+
+
+
+
+
+						This is a multi-line comment in C 
+			
+			Here we are declaring some variables
+			int age = 47; // This is an integer
+			float pi = 3.14; // This is a float
+			char hastag = '#'; // This is a character
+			type of variables age and pi are int and float respectively
+			in the variable hastag we are storing a character
+			so basically we stored 47 in as an integer in age
+			we stored 3.14 as a float in pi
+			and we stored '#' as a character in hastag
+			we can also store a string in a variable
+			we will learn about strings later
+			see the program below.
+
+
+
+
+
+
+	 */
+
+
+// ** idk if you understand the concept of variables or not but i'll explain it to you
+// ** variables are like containers in which we store data
+// ** we can store any type of data in a variable
+
+	int age = 47;
+	float pi = 3.14;
+	char hastag = '#';
+	printf("Age: %d\n", age);
+	printf("Pi: %f\n", pi);
+	printf("Hastag: %c\n", hastag);
+
+
+
+// -------------------------------------------------------------------------------------------------------------------//
+
+
+
+
+
+	// printf() is a function which is used to display a message on the screen 
+	// Antyhing inside the double quotes along with paranthesis will be displayed the message on the screen
+
+
+
+//-------------------------------------------------------------------------------------------------------------------//
+
+
+//    										\n is used to print a new line
+
+
+//-------------------------------------------------------------------------------------------------------------------//
+
+printf("Welcome in the Sourge Developers Community\niemafzalhassan is the founder of this community\n");
+
+
+printf("age = 29\n");
+
+printf("My age is %d\n", age);
+// %d means double value which is used to print or display interger on the screen.
+
+printf("The value for pie is %f\n", pi);
+
+// %f is used to display real numbers (float like eg:---> 1.22662, 1672617.272).
+
+printf("Hashtag %c\n", hastag);
+
+// %c is used to display character's like eg:---> *, # 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
+
+
+// this code is contributed by iemafzalhassan 
+// now below we'll study data type work flow in c.
+
