7d44c5ff2cc28431deb2ed9d4b28c6b63349ec7d
Md. Afzal Hassan Ehsani <iemafzalhassan@gmail.com>
2023-10-29 16:10:15 -0400

    learned range loop any more

---
+++ python/pythonProject/app.py
@@ -0,0 +1,156 @@
+# age = 20
+# price = 21.21
+# firstName = "Afzal"
+# isOnline = False
+# print(age)
+
+#
+# firstName = "John"
+# secondName = "Smith"
+# age = 20
+# isNewPatient = True
+# print("Mr " + firstName,"is Old Patient", age,"With Aged.")
+
+
+#
+# name = input("what is your name? ")
+# print("Hello " + name)
+
+#
+# birth_year = input("Enter your Age ")
+# age = 2023 - int(birth_year)
+# print(age)
+
+
+
+# int()
+# float()
+# bool()
+# str()
+
+#  sum cal
+
+
+# first = input("Enter your First Number: ")
+# second = input("Enter your Second Number")
+# sum = float(first) + float(second)
+# print("Sum is : " + str(sum))
+#
+
+#  Also can be solved
+
+# first = float(input("Enter your First Number: "))
+# second = float(input("Enter your Second Number"))
+# sum = first + second
+# print("Sum is : " + str(sum))
+
+
+# course = "Python for beginner's "
+# print(course.find('y'))
+# print(course.lower())
+# print(course)
+
+# Operators
+
+# print(10+11)
+# print(10*11)
+# print(10/11)
+# print(10%11)
+# print(10**11)
+
+
+# x = 21211
+# x = x + 21
+# print(x)
+
+# x = 21211
+# x += 21
+# print(x)
+
+# x = 1 + 3 * 2
+# # operator precedance
+# print(x)
+#
+
+
+# x = (1 + 3) * 2
+# print(x)
+
+#  Comparison Operator
+
+# x = 3 > 2
+# print(x)
+
+# x = 3 < 2
+# print(x)
+
+
+# x = 3 <= 2
+# print(x)
+
+
+
+# x = 3 >= 2
+# print(x)
+
+
+# x = 3 == 2
+# print(x)
+
+
+# x = 3 != 2
+# print(x)
+
+# Logical operator
+
+# price = 32.21
+# print(price > 30 and price < 35)
+
+
+
+# price = 5
+# print(price > 30 or price < 35)
+
+
+
+# price = 6
+# price(not price > 10)
+
+# If statement to make decision
+
+
+
+
+# temp = float(input("Enter the temp : "))
+# if temp > 30:
+#     print("it's hot outside today! ")
+# elif temp > 20:
+#     print("it's a bit cold outside! ")
+# elif temp > 10:
+#     print("it's cold outside! ")
+# else:
+#     print("It's Cold! ")
+# print("Done")
+#
+#
+#
+# weight = float(input("Enter your Weight: "))
+# unit = (input("(K)g or (L)bs: "))
+# if unit.upper() == "K":
+#     converted = weight / 0.45
+#     print("Weight in Lbs:" + str(converted))
+# else:
+#     converted = weight * 0.45
+#     print("Weight in kgs: " + str(converted))
+#
+#
+
+
+
+
+
+
+
+
+
+
